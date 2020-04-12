394814f6c1bd961e825755d1dcdd6615711d0e19
Ugur Kaya <49519312+ugurcankaya@users.noreply.github.com>
2020-04-13 02:39:56 +0300

    Create arrays.swift

---
+++ arrays.swift
@@ -0,0 +1,14 @@
+//Arrays
+var emptyArray:[Int] = []
+emptyArray.count //returns length of array
+emptyArray.last // last val of array
+emptyArray.append(21)
+emptyArray.first // first val of array
+emptyArray.insert(20, at: 0) // val, index expected
+print(emptyArray)
+emptyArray.sort() // sort ascending
+emptyArray.reverse() // reverse
+emptyArray.shuffle() // changes indexes randomly
+
+
+
