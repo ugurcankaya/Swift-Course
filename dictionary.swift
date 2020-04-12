7271ca0e5348bf5d8df2a39fda775dd11e4ca2f2
Ugur Kaya <49519312+ugurcankaya@users.noreply.github.com>
2020-04-13 02:57:20 +0300

    Create dictionary.swift

---
+++ dictionary.swift
@@ -0,0 +1,12 @@
+//Dictionaries
+    //Key: Value
+    //Constant Time (Performance)
+let emptyDictionary: [String:Int] = [:]   // [Type:Type]
+let devices: [String:String] = [
+    "phone": "iPhone 7",
+    "laptop" : "2017 Macbook Pro",
+    "tablet" : "2019 iPad",
+    "desktop" : "Cooler Master PC"
+]
+
+devices["laptop"] //returns : 2017 Macbook Pro
