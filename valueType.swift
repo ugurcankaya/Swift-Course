155233741f7a52faf279ecf75f9dbe6c525e4b7c
Ugur Kaya <49519312+ugurcankaya@users.noreply.github.com>
2020-04-13 18:56:54 +0300

    Update valueType.swift

--- valueType.swift
+++ valueType.swift
@@ -10,5 +10,5 @@ var Ugur = Developer(name: "Ugur", jobTitle: "Junior", yearsExp: 0)
 print(Ugur.name)
 var Sean = Ugur
 Sean.name = "Sean" // doesn't affect Ugur's reference because struct is a value type variable unlike classes
-print(Ugur.name)
-print(Sean.name)
+print(Ugur.name) //returns Ugur
+print(Sean.name) //returns Sean
