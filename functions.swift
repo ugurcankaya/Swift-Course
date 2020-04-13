fabb2e65213ea17da4325ed102a28f86f01ada79
Ugur Kaya <49519312+ugurcankaya@users.noreply.github.com>
2020-04-13 17:07:39 +0300

    Create functions.swift

---
+++ functions.swift
@@ -0,0 +1,7 @@
+//Functions
+
+func add(firstNumber:Int, to secondNumber:Int)->Int{ //to is used as label, label will be used by user but in functions parameterName is.
+    return firstNumber+secondNumber
+}
+
+add(firstNumber: 5, to: 5)
