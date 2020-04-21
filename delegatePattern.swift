f11a8bb5c43dc99498a2627d2bdf2a99b0e9b782
Ugur Kaya <49519312+ugurcankaya@users.noreply.github.com>
2020-04-22 00:01:42 +0300

    v2

--- delegatePattern.swift
+++ delegatePattern.swift
@@ -21,9 +21,36 @@ struct Paramedic: AdvancedLifeSupport{
         print("CPR will have been performed to patient asap.")
     }
 }
-let emilio = EmergencyCallHander()
-let pete = Paramedic(handler: emilio)
 
+class Doctor:AdvancedLifeSupport{
+    init(handler: EmergencyCallHander){
+        handler.delegate = self
+    }
+    
+    func performCPR() {
+        print("Doctor will be performing cpr.")
+    }
+    
+    func useStathescope(){
+        print("Listen heart")
+    }
+}
+
+class Surgeon : Doctor {
+    //it has AdvancedLifeSupport and have the function from super class doctor.
+    override func performCPR() {
+        super.performCPR()
+        electricDrill()
+        print("It was performed by a Surgeon.")
+    }
+    func electricDrill(){
+        print("Whir...")
+    }
+}
+
+let emilio = EmergencyCallHander()
+//let pete = Paramedic(handler: emilio)
+let angela = Surgeon(handler: emilio)
 emilio.assessSituation()
 emilio.medicalEmergency()
 
